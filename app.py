a50785f877a2dec3a7701e4c19d634141053ea90
Grant Stoltman <gstoltman@gmail.com>
2023-06-21 20:11:08 -0700

    first draft done, need to work on uploading to domain and improving UI

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, url_for, flash, redirect
-# import create_fake_data
+from flask import Flask, render_template, request, url_for, flash, redirect, Response
+import pandas as pd
+from main import generate_data
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'a7d221fcc7abdc1e3b7442b13bd060e69b70da9309c0b0c2'
@@ -8,16 +9,34 @@ app.config['SECRET_KEY'] = 'a7d221fcc7abdc1e3b7442b13bd060e69b70da9309c0b0c2'
 def index():
     return render_template('index.html')
 
-# START HERE NEXT TIME< GET WEBSITE INTAKE WORKING WITH CREATE FUNCTION
-
-@app.route('/', methods=(['POST']))
+@app.route('/', methods=(['GET', 'POST']))
 def process_form():
-    inputs = []
     category1 = request.form['category1']
     category2 = request.form['category2']
     category3 = request.form['category3']
+
+    category_list = [category1, category2, category3]
     row_count = int(request.form['row_count'])
 
-    flash([category1, category2, category3, row_count])
+    global list_of_lists 
+    list_of_lists = [[category1, category2, category3]]
+    for row in range(row_count):
+        generated_data = generate_data(category_list)
+        list_of_lists.append(generated_data)
+
+    return render_template('index.html', matrix=list_of_lists)
     return redirect(url_for('index'))
-    return render_template('index.html')
+
+@app.route('/export', methods=['POST'])
+def export_csv():
+    # Sample data to export
+    df = pd.DataFrame(list_of_lists)
+
+    # Create a response object with CSV content
+    response = Response(df.to_csv(index=False), mimetype='text/csv')
+    response.headers['Content-Disposition'] = 'attachment; filename=data.csv'
+    return response
+
+
+if __name__ == '__main__':
+    app.run()
--- main.py
+++ main.py
@@ -4,29 +4,10 @@ from faker import Faker
 
 fake = Faker()
 
-# ADD CATEGORIES TO DICT AND FAKE DATA CALL !!!
-
-# placeholder, need to create code to intake data from GUI
-
-
-# placeholder, need to create code to intake data from GUI
-
-
-def create_fake_data(row_count):
-    columns_info = {
-    'random_int': {'category': 'random_int', 'datatype': 'integer'},
-    'name': {'category': 'name', 'datatype': 'text'},
-    'country': {'category': 'country', 'datatype': 'text'}
-    }
-
-    column_names = list(columns_info.keys())
-
-    df = pd.DataFrame(columns=column_names)
-
-    for outer_key, inner_dict in columns_info.items():
-        method = getattr(fake, outer_key)
-        fake_data = [method() for _ in range(row_count)]
-        df[outer_key] = fake_data
-    print(df)
-
-create_fake_data(6)
+def generate_data(column_list):
+    row_list = []
+    for i in range(len(column_list)):
+        method = getattr(fake, column_list[i])
+        generated_value = method()
+        row_list.append(generated_value)
+    return row_list
